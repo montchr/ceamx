8101bc0b2eb4c268c3f38b532dc786dcf858f84b
Chris Montgomery <chris@cdom.io>
2022-12-17 23:09:01 -0500

    init

---
+++ init.el
@@ -0,0 +1,25 @@
+;;; init.el --- Ceamachs Init -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2022 Chris Montgomery
+;;
+;; Author: Chris Montgomery <chris@cdom.io>
+;; Maintainer: Chris Montgomery <chris@cdom.io>
+;; Created: December 17, 2022
+;; Modified: December 17, 2022
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
+;; Homepage: https://github.com/montchr/init
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Ceamachs Init
+;;
+;;; Code:
+
+
+
+(provide 'init)
+;;; init.el ends here
