ff788b66dda02907cda1f0227fab239d8ed69550
Chris Montgomery <chris@cdom.io>
2023-01-19 12:03:06 -0500

    chore(reuse|init): add gpl-3.0 license header to benchmarking file

--- lisp/init-benchmarking.el
+++ lisp/init-benchmarking.el
@@ -1,8 +1,25 @@
 ;;; init-benchmarking.el --- Measure startup and require times -*- lexical-binding: t -*-
+
+;; Copyright (c) 2022-2023  Chris Montgomery <chris@cdom.io>
+
+;; This file is NOT part of GNU Emacs.
+
+;; This file is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the
+;; Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
 ;;
+;; This file is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this file.  If not, see <http://www.gnu.org/licenses/>.
+
 ;; Copyright (c) 2006-2021, Steve Purcell
 ;; All rights reserved.
-;;
+
 ;; Redistribution and use in source and binary forms, with or without
 ;; modification, are permitted provided that the following conditions are met:
 ;;
@@ -22,7 +39,7 @@
 ;; ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 ;; (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 ;; SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-;;
+
 ;;; Commentary:
 ;;; Code:
 
