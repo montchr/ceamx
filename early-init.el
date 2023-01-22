9923ef4c218e352a3584be16a415275069734a52
Chris Montgomery <chris@cdom.io>
2023-01-22 15:04:15 -0500

    gc: disable post-init settings message

--- early-init.el
+++ early-init.el
@@ -116,7 +116,8 @@ Use this for files that change often, like data and cache files.")
      (setq file-name-handler-alist default-file-name-handler-alist
            gc-cons-percentage 0.1
            gc-cons-threshold 100000000)
-     (message "gc-cons-threshold & file-name-handler-alist restored")
+     ;; TODO: debug mode flag
+     ;; (message "gc-cons-threshold & file-name-handler-alist restored")
      (when (boundp 'after-focus-change-function)
        (add-function :after after-focus-change-function #'+gc-after-focus-change)))))
 (with-eval-after-load 'elpaca
