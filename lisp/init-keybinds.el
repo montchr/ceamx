d65356ee70a283c722f7ca50a9ba98c07358e11c
Chris Montgomery <chris@cdom.io>
2023-07-08 14:33:38 -0400

    keybinds: universal non-nuclear escape via doom

--- lisp/init-keybinds.el
+++ lisp/init-keybinds.el
@@ -74,6 +74,54 @@
                       [C-i] [?\C-i])))
 
 ;;
+;;; Universal, non-nuclear escape
+;;  Source: <https://github.com/doomemacs/doomemacs/blob/07fca786154551f90f36535bfb21f8ca4abd5027/lisp/doom-keybinds.el#L70-L113>
+
+;; `keyboard-quit' is too much of a nuclear option.
+;; The following defines a ESC/C-g DWIM alternative
+;; It serves four purposes (in order):
+;;
+;; 1. Quit active states; e.g. highlights, searches, snippets, iedit,
+;;    multiple-cursors, recording macros, etc.
+;; 2. Close popup windows remotely (if it is allowed to)
+;; 3. Refresh buffer indicators, like git-gutter and flycheck
+;; 4. Or fall back to `keyboard-quit'
+;;
+;; And it should do these things incrementally, rather than all at once. And it
+;; shouldn't interfere with recording macros or the minibuffer. This may require
+;; you press ESC/C-g two or three times on some occasions to reach
+;; `keyboard-quit', but this is much more intuitive.
+
+(defvar cmx-escape-hook nil
+  "A hook run when C-g is pressed (or ESC in normal mode, for evil users).
+
+More specifically, when `cmx/escape' is pressed. If any hook returns non-nil,
+all hooks after it are ignored.")
+
+(defun cmx/escape (&optional interactive)
+  "Run `cmx-escape-hook'."
+  (interactive (list 'interactive))
+  (cond ((minibuffer-window-active-p (minibuffer-window))
+         ;; quit the minibuffer if open.
+         (when interactive
+           (setq this-command 'abort-recursive-edit))
+         (abort-recursive-edit))
+        ;; Run all escape hooks. If any returns non-nil, then stop there.
+        ((run-hook-with-args-until-success 'cmx-escape-hook))
+        ;; don't abort macros
+        ((or defining-kbd-macro executing-kbd-macro) nil)
+        ;; Back to the default
+        ((unwind-protect (keyboard-quit)
+           (when interactive
+             (setq this-command 'keyboard-quit))))))
+
+(global-set-key [remap keyboard-quit] #'cmx/escape)
+
+(with-eval-after-load 'eldoc
+  (eldoc-add-command 'cmx/escape))
+
+
+;;
 ;;; === GENERAL.EL =============================================================
 ;;  
 
