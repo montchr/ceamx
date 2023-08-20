91b756f3ff1c4dca730203adefa3aaad24d73a5d
Chris Montgomery <chris@cdom.io>
2023-08-20 14:18:19 -0400

    fix(theme|custom): remove customised safe themes to allow for all
    This setting in `custom.el` was overriding the `t` value set in `init-ui-theme`.

--- custom.el
+++ custom.el
@@ -3,8 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("eb50f36ed5141c3f702f59baa1968494dc8e9bd22ed99d2aaa536c613c8782db" "4320a92406c5015e8cba1e581a88f058765f7400cf5d885a3aa9b7b9fc448fa7" "bf64489b417d5a0e424f848df3652d89fafeaa299cf98922e77cbfba2c1b50d6" "bc2936e8cd9c3e67623e76672ddf53411e60723e2ed0cad8b4ca59b5a2d80bbf" "cca1d386d4a3f645c2f8c49266e3eb9ee14cf69939141e3deb9dfd50ccaada79" "64947f83c308d1101f20c6ff86c308497c4d032acb75d8e9dcbc4012d9a3b36e" "277a5bce12d6957dbabb43a2f55ee2b6371388b749cbb29fd251df19334a1f0b" "1e2f562077d76160ad2a4b648bf754698ab8cdf3914db933badc356881dad147" "a9c9f34794fa0b2da9e111a85402fb3f9b82e281398e0249bbc073210592e198" "f82e68d489e6c21c9552c4e8e35a03d126d9eba632a8e7b4f9329d1374b4a19c" "d395c1793e0d64797d711c870571a0033174ca321ed48444efbe640bf692bf4f" "11873c4fbf465b956889adfa9182495db3bf214d9a70c0f858f07f6cc91cbd47" "eb7cd622a0916358a6ef6305e661c6abfad4decb4a7c12e73d6df871b8a195f8" default))
+ '(custom-enabled-themes '(modus-operandi-tinted))
  '(newsticker-url-list
    '(("Human Transit" "https://humantransit.org/feed" nil nil nil)
      ("Hacker News" "https://news.ycombinator.com/rss" nil nil nil)
