c80a7608530df1c8d065812be9e6e35bbf8c0476
Chris Montgomery <chris@cdom.io>
2023-08-29 11:15:30 -0400

    fix(treemacs): settings tweak

--- lisp/init-ui-treemacs.el
+++ lisp/init-ui-treemacs.el
@@ -28,83 +28,85 @@
 
 (use-package treemacs
   :defer t
-  :init
-  (with-eval-after-load 'winum
-    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
   :config
-  (progn
-    (setq treemacs-collapse-dirs                   (if treemacs-python-executable 3 0)
-          treemacs-deferred-git-apply-delay        0.5
-          treemacs-directory-name-transformer      #'identity
-          treemacs-display-in-side-window          t
-          treemacs-eldoc-display                   'simple
-          treemacs-file-event-delay                2000
-          treemacs-file-extension-regex            treemacs-last-period-regex-value
-          treemacs-file-follow-delay               0.2
-          treemacs-file-name-transformer           #'identity
-          treemacs-follow-after-init               t
-          treemacs-expand-after-init               t
-          treemacs-find-workspace-method           'find-for-file-or-pick-first
-          treemacs-git-command-pipe                ""
-          treemacs-goto-tag-strategy               'refetch-index
-          treemacs-header-scroll-indicators        '(nil . "^^^^^^")
-          treemacs-hide-dot-git-directory          t
-          treemacs-indentation                     2
-          treemacs-indentation-string              " "
-          treemacs-is-never-other-window           t
-          treemacs-max-git-entries                 5000
-          treemacs-missing-project-action          'ask
-          treemacs-move-forward-on-expand          nil
-          treemacs-no-png-images                   t
-          treemacs-no-delete-other-windows         t
-          treemacs-project-follow-cleanup          nil
-          ;; FIXME: should already be handled by `no-littering', but needs confirmation
-          ;; treemacs-persist-file                    (expand-file-name "treemacs-persist" +path-local-dir)
-          treemacs-position                        'left
-          treemacs-read-string-input               'from-child-frame
-          treemacs-recenter-distance               0.1
-          treemacs-recenter-after-file-follow      nil
-          treemacs-recenter-after-tag-follow       nil
-          treemacs-recenter-after-project-jump     'always
-          treemacs-recenter-after-project-expand   'on-distance
-          treemacs-litter-directories              '("node_modules" "vendor" "result" ".venv" ".cask")
-          treemacs-project-follow-into-home        nil
-          treemacs-show-cursor                     nil
-          treemacs-show-hidden-files               t
-          treemacs-silent-filewatch                nil
-          treemacs-silent-refresh                  nil
-          treemacs-sorting                         'alphabetic-case-insensitive-asc
-          treemacs-select-when-already-in-treemacs 'move-back
-          treemacs-space-between-root-nodes        t
-          treemacs-tag-follow-cleanup              t
-          treemacs-tag-follow-delay                1.5
-          treemacs-text-scale                      nil
-          treemacs-user-mode-line-format           nil
-          treemacs-user-header-line-format         nil
-          treemacs-wide-toggle-width               70
-          treemacs-width                           35
-          treemacs-width-increment                 1
-          treemacs-width-is-initially-locked       t
-          treemacs-workspace-switch-cleanup        nil)
+  (setq treemacs-collapse-dirs                   (if treemacs-python-executable 3 0))
+  (setq treemacs-deferred-git-apply-delay        0.5)
+  (setq treemacs-directory-name-transformer      #'identity)
+  (setq treemacs-display-in-side-window          t)
+  (setq treemacs-eldoc-display                   'simple)
+  (setq treemacs-file-event-delay                2000)
+  (setq treemacs-file-extension-regex            treemacs-last-period-regex-value)
+  (setq treemacs-file-follow-delay               0.2)
+  (setq treemacs-file-name-transformer           #'identity)
+  (setq treemacs-follow-after-init               t)
+  (setq treemacs-expand-after-init               t)
+  (setq treemacs-find-workspace-method           'find-for-file-or-pick-first)
+  (setq treemacs-git-command-pipe                "")
+  (setq treemacs-goto-tag-strategy               'refetch-index)
+  (setq treemacs-header-scroll-indicators        '(nil . "^^^^^^"))
+  (setq treemacs-hide-dot-git-directory          t)
+  (setq treemacs-indentation                     2)
+  (setq treemacs-indentation-string              " ")
+  (setq treemacs-is-never-other-window           t)
+  (setq treemacs-max-git-entries                 5000)
+  (setq treemacs-missing-project-action          'ask)
+  (setq treemacs-move-forward-on-expand          nil)
+  (setq treemacs-no-png-images                   t)
+  (setq treemacs-no-delete-other-windows         t)
+  (setq treemacs-project-follow-cleanup          t)
+  ;; FIXME: should already be handled by `no-littering', but needs confirmation
+  ;; treemacs-persist-file                    (expand-file-name "treemacs-persist" +path-local-dir)
+  (setq treemacs-position                        'left)
+  (setq treemacs-read-string-input               'from-child-frame)
+  (setq treemacs-recenter-distance               0.1)
+  (setq treemacs-recenter-after-file-follow      nil)
+  (setq treemacs-recenter-after-tag-follow       nil)
+  (setq treemacs-recenter-after-project-jump     'always)
+  (setq treemacs-recenter-after-project-expand   'on-distance)
+  (setq treemacs-litter-directories              '("node_modules" "vendor" "result" ".venv" ".cask"))
+  (setq treemacs-project-follow-into-home        nil)
+  (setq treemacs-show-cursor                     nil)
+  (setq treemacs-show-hidden-files               t)
+  (setq treemacs-silent-filewatch                nil)
+  (setq treemacs-silent-refresh                  nil)
+  (setq treemacs-sorting                         'alphabetic-case-insensitive-asc)
+  (setq treemacs-select-when-already-in-treemacs 'move-back)
+  (setq treemacs-space-between-root-nodes        t)
+  (setq treemacs-tag-follow-cleanup              t)
+  (setq treemacs-tag-follow-delay                1.5)
+  (setq treemacs-text-scale                      nil)
+  (setq treemacs-user-mode-line-format           nil)
+  (setq treemacs-user-header-line-format         nil)
+  (setq treemacs-wide-toggle-width               70)
+  (setq treemacs-width                           35)
+  (setq treemacs-width-increment                 1)
+  (setq treemacs-width-is-initially-locked       t)
+  (setq treemacs-workspace-switch-cleanup        nil)
 
-    ;; The default width and height of the icons is 22 pixels. If you are
-    ;; using a Hi-DPI display, uncomment this to double the icon size.
-    ;;(treemacs-resize-icons 44)
+  ;; The default width and height of the icons is 22 pixels. If you are
+  ;; using a Hi-DPI display, uncomment this to double the icon size.
+  ;;(treemacs-resize-icons 44)
 
-    (treemacs-follow-mode t)
-    (treemacs-filewatch-mode t)
-    (treemacs-fringe-indicator-mode 'always)
-    (when treemacs-python-executable
-      (treemacs-git-commit-diff-mode t))
+  (treemacs-follow-mode t)
+  (treemacs-filewatch-mode t)
+  (treemacs-fringe-indicator-mode 'always)
+  (when treemacs-python-executable
+    (treemacs-git-commit-diff-mode t))
 
-    (pcase (cons (not (null (executable-find "git")))
-                 (not (null treemacs-python-executable)))
-      (`(t . t)
-       (treemacs-git-mode 'deferred))
-      (`(t . _)
-       (treemacs-git-mode 'simple)))
+  (pcase (cons (not (null (executable-find "git")))
+               (not (null treemacs-python-executable)))
+    (`(t . t)
+     (treemacs-git-mode 'deferred))
+    (`(t . _)
+     (treemacs-git-mode 'simple)))
 
-    (treemacs-hide-gitignored-files-mode nil))
+
+
+  (treemacs-hide-gitignored-files-mode nil)
+
+  (defvar-keymap cmx-explore-keymap
+    "t" #'treemacs)
+
 
   :bind
   ;; TODO: add to new sidebar keymap in `init-keys'
