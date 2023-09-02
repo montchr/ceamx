8c54e020b312839d341ffdcd85f0a6caac0d959a
Chris Montgomery <chris@cdom.io>
2023-09-02 15:44:50 -0400

    fix(buffer): hide empty async buffer on init

--- lisp/init-buffers.el
+++ lisp/init-buffers.el
@@ -31,7 +31,12 @@
   :init
   ;; Disable buffer line wrapping by default.
   ;; <https://www.emacswiki.org/emacs/TruncateLines>
-  (set-default 'truncate-lines t))
+  (set-default 'truncate-lines t)
+  :config
+  ;; Hide buffer until there's output.
+  ;; Prevents an extra window appearing during init.
+  (setq async-shell-command-display-buffer nil))
+
 
 ;; (use-package bufler
 ;;   :elpaca (bufler :host github :repo "alphapapa/bufler.el"
