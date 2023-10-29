18857543e6efee88a8df0b3afba0636bf6632835
Chris Montgomery <chris@cdom.io>
2023-10-29 19:31:24 -0400

    feat(ide): add dumb-jump

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -25,11 +25,41 @@
 
 ;;  Configuration for LSP servers.
 
+;; FIXME: make this more generalized for "IDE" stuff, not just LSP
+
 ;;; Code:
 
 (defvar +lsp-defer-shutdown 3)
 
 (autoload '+lsp-defer-server-shutdown-a "lib-lsp")
+
+;;
+;;; `dumb-jump' :: <https://github.com/jacktasia/dumb-jump>
+;;
+;;  "zero-configuration" jump-to-definition package with support for many langs
+
+(use-package dumb-jump
+  :autoload dumb-jump-xref-activate
+  :init
+  (add-hook 'xref-backend-functions #'dumb-jump-xref-activate)
+  :config
+  (setopt dumb-jump-force-searcher 'rg))
+
+(after! [hydra dumb-jump]
+  ;; via <https://github.com/jacktasia/dumb-jump?tab=readme-ov-file#hydra-for-effieciency>
+  (defhydra dumb-jump-hydra (:color blue :columns 3)
+    "Dumb Jump"
+    ("j" dumb-jump-go "Go")
+    ("o" dumb-jump-go-other-window "Other window")
+    ("e" dumb-jump-go-prefer-external "Go external")
+    ("x" dumb-jump-go-prefer-external-other-window "Go external other window")
+    ("i" dumb-jump-go-prompt "Prompt")
+    ("l" dumb-jump-quick-look "Quick look")
+    ("b" dumb-jump-back "Back")))
+
+;;
+;;; `lsp-mode' :: <https://emacs-lsp.github.io/lsp-mode/>
+;;
 
 (use-package lsp-mode
   :commands (lsp-deferred lsp-execute-code-action)
