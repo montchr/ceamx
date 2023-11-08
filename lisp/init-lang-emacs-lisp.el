bdac4fed6235708974bf5f0ab0b33d7a37f7d36f
Chris Montgomery <chris@cdom.io>
2023-11-08 13:17:25 -0500

    ch(elisp): turns out i have been yearning for a return to lispy

--- lisp/init-lang-emacs-lisp.el
+++ lisp/init-lang-emacs-lisp.el
@@ -69,6 +69,43 @@
 ;;   ;; (add-hook 'flycheck-mode-hook #'+emacs-lisp-non-package-mode)
 ;;   )
 
+;;; `lispy' :: <https://github.com/abo-abo/lispy>
+(use-package lispy
+  :config
+  (dolist (mode cmx-lisp-mode-list)
+    (let ((hook (intern (format "%S-hook" mode))))
+      (add-hook hook (cmd! (lispy-mode +1)))))
+
+  ;; Prevent `lispy' from inserting escaped quotes when already inside a string,
+  ;; in favor of just moving past the closing quote as I would expect.
+  (setopt lispy-close-quotes-at-end-p t)
+
+  ;; TODO: Remove after <https://github.com/abo-abo/lispy/pull/619> (if ever?)
+  ;; (keymap-unset lispy-mode-map "`" t) ; <- does not work. why not?
+  (keymap-set lispy-mode-map "`"   #'self-insert-command)
+
+  (keymap-set lispy-mode-map "M-v" nil))
+
+;;; `lispyville' :: <https://github.com/noctuid/lispyville>
+(use-package lispyville
+  :after (evil lispy)
+  :defines (lispyville-key-theme)
+
+  :init
+  ;; via doom
+  (setq lispyville-key-theme
+        '((operators normal)
+          c-w
+          (prettify insert)
+          (atom-movement t)
+          slurp/barf-lispy
+          additional
+          additional-insert))
+  (add-hook 'lispy-mode-hook #'lispyville-mode)
+
+  :config
+  (lispyville-set-key-theme))
+
 ;;
 ;;; Keybinds
 ;;
