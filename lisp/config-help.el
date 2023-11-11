72b5a16f555cc980e322bf237c9e06448ab41ebd
Chris Montgomery <chris@cdom.io>
2023-11-11 12:06:55 -0500

    fix(help): commit config-help

---
+++ lisp/config-help.el
@@ -0,0 +1,35 @@
+;;; config-help.el --- Help settings                 -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023  Chris Montgomery
+
+;; Author: Chris Montgomery <chris@cdom.io>
+;; Keywords: local, help
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Settings for helping.
+
+;;; Code:
+
+(require 'config-paths)
+
+(defvar cmx-cheatsheets-dir
+  (file-name-as-directory
+   (concat cmx-home-dir "Documents/cheatsheets"))
+  "Absolute path to the directory containing user cheatsheets.")
+
+(provide 'config-help)
+;;; config-help.el ends here
