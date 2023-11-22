87987472953706cfce3eadbe414d6b4fc4956286
Chris Montgomery <chris@cdom.io>
2023-11-22 13:54:24 -0500

    add(nix): nixfmt replaces alejandra one day

--- flake.nix
+++ flake.nix
@@ -1,28 +1,27 @@
 {
-  description = "emacs config flake";
+  description = "ceamx: an emacs config";
 
   inputs.std.url = "github:divnix/std";
   inputs.std.inputs.nixpkgs.follows = "nixpkgs";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.nixpkgs-unfree.url = "github:numtide/nixpkgs-unfree";
-  inputs.nixpkgs-unfree.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.nil-lsp.url = "github:oxalica/nil";
+  inputs.nixfmt.url = "github:serokell/nixfmt";
 
-  outputs = {
-    std,
-    self,
-    ...
-  } @ inputs:
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { std, self, ... }@inputs:
     std.growOn {
       inherit inputs;
       cellsFrom = ./nix;
       cellBlocks = with std.blockTypes; [
-        (installables "packages" {ci.build = true;})
-        (devshells "devshells" {ci.build = true;})
+        (installables "packages" { ci.build = true; })
+        (devshells "devshells" { ci.build = true; })
       ];
-    }
-    {
-      devShells = std.harvest self ["_automation" "devshells"];
-      formatter = inputs.nixpkgs.alejandra;
+    } {
+      devShells = std.harvest self [ "_automation" "devshells" ];
+      # FIXME: std is non-intuitive how the hell does this work
+      # formatter = inputs.nixfmt.packages.default;
+      formatter."aarch64-darwin" =
+        inputs.nixfmt.packages."aarch64-darwin".default;
     };
 }
--- nix/_automation/devshells.nix
+++ nix/_automation/devshells.nix
@@ -1,26 +1,23 @@
-{
-  inputs,
-  cell,
-}: let
+{ inputs, cell, }:
+let
   l = inputs.nixpkgs.lib // builtins;
-  inherit (inputs) nixpkgs std;
-in
-  l.mapAttrs (_: std.lib.dev.mkShell) {
-    default = {...}: {
-      name = "ceamx";
-      commands = [
-        {
-          package = nixpkgs.treefmt;
-          category = "repo tools";
-        }
-        {
-          package = nixpkgs.alejandra;
-          category = "repo tools";
-        }
-        {
-          package = std.std.cli.default;
-          category = "repo tools";
-        }
-      ];
-    };
-  }
+  inherit (inputs) nixpkgs std nixfmt;
+in l.mapAttrs (_: std.lib.dev.mkShell) {
+  default = { ... }: {
+    name = "ceamx";
+    commands = [
+      {
+        package = nixpkgs.treefmt;
+        category = "repo tools";
+      }
+      {
+        package = nixfmt.packages.default;
+        category = "repo tools";
+      }
+      {
+        package = std.std.cli.default;
+        category = "repo tools";
+      }
+    ];
+  };
+}
