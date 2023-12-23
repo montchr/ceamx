98fa2b99a6e65a69cc95fd47b0338aa6737bd328
Chris Montgomery <chris@cdom.io>
2023-12-23 17:49:43 -0500

    fix: use correct format for setting nix formatter

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
 ;;; Directory Local Variables         -*- no-byte-compile: t; -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nix-mode . ((nix-format-nixfmt-on-save-mode . 1))))
+((nix-mode . ((mode . nix-format-nixfmt-on-save))))
