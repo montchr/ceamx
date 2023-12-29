a75207ea3fdedd473861849ea3c405ee5adbfa1c
Chris Montgomery <chris@cdom.io>
2023-12-28 22:40:17 -0500

    tweak(meow): increase hint removal delay

--- lisp/init-keys-meow.el
+++ lisp/init-keys-meow.el
@@ -156,6 +156,11 @@
 
   ;; TODO: evaluate -- i think i prefer this, but need a refresher
   ;; (setopt meow-keypad-leader-dispatch "C-c")
+  ;; Because I'm still getting the hang of meow again. I'm not fast enough to
+  ;; remember exactly what to do. Once this delay feels too long, then it can be
+  ;; changed.
+  (setopt meow-expand-hint-remove-delay 10.0)
+
 
   ;; TODO: investigate effects -- copied from <https://github.com/chuxubank/cat-emacs/blob/65155f642b336d14ca63f010ff45eea2c18cfdce/cats/%2Bmeow.el>
   ;; (setopt meow-expand-exclude-mode-list nil)
