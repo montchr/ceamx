3f52c9a3e2750b1f328f7b3c53307df4565868d9
Chris Montgomery <chris@cdom.io>
2023-12-28 23:06:27 -0500

    fix(vcs:magit|project): revert buffers + clear cache on checkout

--- lisp/init-vcs.el
+++ lisp/init-vcs.el
@@ -58,42 +58,42 @@
   (after! [evil-collection]
     (evil-collection-init 'magit))
 
-  (setopt magit-diff-refine-hunk t)                  ; show granular diffs in selected hunk
-  (setopt magit-save-repository-buffers nil)         ; avoid side-effects (e.g. auto-format)
-  (setopt magit-revision-insert-related-refs nil)    ; parent/related refs: rarely useful
-  (setopt magit-process-finish-apply-ansi-colors t)  ; render ANSI colors in process output
+  (setopt magit-diff-refine-hunk t)     ; show granular diffs in selected hunk
+  (setopt magit-save-repository-buffers nil) ; avoid side-effects (e.g. auto-format)
+  (setopt magit-revision-insert-related-refs nil) ; parent/related refs: rarely useful
+  (setopt magit-process-finish-apply-ansi-colors t) ; render ANSI colors in process output
 
   ;; Close transient with ESC
   (define-key transient-map [escape] #'transient-quit-one)
 
-  ;; FIXME: on `on-switch-buffer-hook'
-  ;; (defadvice! +magit-revert-repo-buffers-deferred-a (&rest _)
-  ;;   :after '(magit-checkout magit-branch-and-checkout)
-  ;;   ;; Since the project likely now contains new files, best we undo the
-  ;;   ;; projectile cache so it can be regenerated later.
-  ;;   (projectile-invalidate-cache nil)
-  ;;   ;; Use a more efficient strategy to auto-revert buffers whose git state has
-  ;;   ;; changed: refresh the visible buffers immediately...
-  ;;   (+magit-mark-stale-buffers-h))
-  ;; ;; ...then refresh the rest only when we switch to them, not all at once.
-  ;; (add-hook 'doom-switch-buffer-hook #'+magit-revert-buffer-maybe-h)
+  (defadvice! +magit-revert-repo-buffers-deferred-a (&rest _)
+    "Revert repo buffers and invalidate caches upon checkout."
+    :after '(magit-checkout magit-branch-and-checkout)
+    ;; Since the project likely now contains new files, best we undo the
+    ;; projectile cache so it can be regenerated later.
+    (after! 'projectile
+      (projectile-invalidate-cache nil))
+    ;; Use a more efficient strategy to auto-revert buffers whose git state has
+    ;; changed: refresh the visible buffers immediately...
+    (+magit-mark-stale-buffers-h))
+  ;; ...then refresh the rest only when we switch to them, not all at once.
+  (add-hook 'on-switch-buffer-hook #'+magit-revert-buffer-maybe-h)
 
   ;; Prevent sudden window position resets when staging/unstaging/discarding/etc
   ;; hunks in `magit-status-mode' buffers.
   (defvar +magit--pos nil)
   (add-hook 'magit-pre-refresh-hook
-            (defun +magit--set-window-state-h ()
-              (setq-local +magit--pos (list (current-buffer) (point) (window-start)))))
+    (defun +magit--set-window-state-h ()
+      (setq-local +magit--pos (list (current-buffer) (point) (window-start)))))
   (add-hook 'magit-post-refresh-hook
-            (defun +magit--restore-window-state-h ()
-              (when (and +magit--pos (eq (current-buffer) (car +magit--pos)))
-                (goto-char (cadr +magit--pos))
-                (set-window-start nil (caddr +magit--pos) t)
-                (kill-local-variable '+magit--pos))))
+    (defun +magit--restore-window-state-h ()
+      (when (and +magit--pos (eq (current-buffer) (car +magit--pos)))
+        (goto-char (cadr +magit--pos))
+        (set-window-start nil (caddr +magit--pos) t)
+        (kill-local-variable '+magit--pos))))
 
   ;; <https://magit.vc/manual/magit/Switching-Buffers.html#index-magit_002ddisplay_002dbuffer_002dfullframe_002dstatus_002dv1>
   (setopt magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1)
-
 
   (transient-append-suffix 'magit-fetch "-p"
     '("-t" "Fetch all tags" ("-t" "--tags")))
