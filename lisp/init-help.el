f7dcbd3f44636fd091b352e80620c44b1040719c
Chris Montgomery <chris@cdom.io>
2024-01-26 01:52:53 -0500

    fix(help): make eldoc messages way less shy and more helpful

--- lisp/init-help.el
+++ lisp/init-help.el
@@ -27,6 +27,8 @@
 
 ;;; Code:
 
+(require 'lib-common)
+
 ;; NOTE: there are some blocking bugs that have gone unfixed for quite a while
 ;;        some symbols' helpful pages cannot be displayed.
 ;;        <https://github.com/Wilfred/helpful/issues/329>
@@ -35,6 +37,23 @@
   :defer 2
   :commands ( helpful-at-point helpful-command helpful-callable
               helpful-key helpful-symbol helpful-variable))
+
+(use-feature! eldoc
+  :config
+  ;; via <https://github.com/radian-software/radian/blob/20c0c9d929a57836754559b470ba4c3c20f4212a/emacs/radian.el#L2800-L2810>
+  (def-advice! +eldoc-better-display-message-p-a (&rest _)
+    :override #'eldoc--message-command-p
+    "Make ElDoc smarter about when to display its messages.
+From the original author:
+
+\"By default ElDoc has a customizable whitelist of commands that
+it will display its messages after. The idea of this is to not
+trample on messages that other commands may have printed.
+However, this is a hopeless endeavour because there are a
+virtually unlimited number of commands that don't conflict with
+ElDoc. A better approach is to simply check to see if a message
+was printed, and only have ElDoc display if one wasn't.\""
+    (member (current-message) (list nil eldoc-last-message))))
 
 ;;; `elisp-demos' :: <https://github.com/xuchunyang/elisp-demos>
 ;;  Display usage examples inside help buffers for Emacs Lisp callables.
