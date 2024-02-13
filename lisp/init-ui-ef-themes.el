0e25622664eddcc28c2399b156b0e1fda7972dad
Chris Montgomery <chris@cdom.io>
2024-02-12 20:07:12 -0500

    fix(theme:ef|font): disable awkwardly-sized ef-themes-headings

--- lisp/init-ui-ef-themes.el
+++ lisp/init-ui-ef-themes.el
@@ -29,16 +29,17 @@
   :config
   (setopt ef-themes-to-toggle '(ef-frost ef-winter))
 
-  (setopt ef-themes-headings         ; read the manual's entry or the doc string
-    '((0 variable-pitch light 1.9)
-       (1 variable-pitch light 1.8)
-       (2 variable-pitch regular 1.7)
-       (3 variable-pitch regular 1.6)
-       (4 variable-pitch regular 1.5)
-       (5 variable-pitch 1.4)           ; absence of weight means `bold'
-       (6 variable-pitch 1.3)
-       (7 variable-pitch 1.2)
-       (t variable-pitch 1.1)))
+  (setopt ef-themes-headings nil)
+  ;; (setopt ef-themes-headings         ; read the manual's entry or the doc string
+  ;;   '((0 variable-pitch light 1.9)
+  ;;      (1 variable-pitch light 1.8)
+  ;;      (2 variable-pitch regular 1.7)
+  ;;      (3 variable-pitch regular 1.6)
+  ;;      (4 variable-pitch regular 1.5)
+  ;;      (5 variable-pitch 1.4)           ; absence of weight means `bold'
+  ;;      (6 variable-pitch 1.3)
+  ;;      (7 variable-pitch 1.2)
+  ;;      (t variable-pitch 1.1)))
   ;; They are nil by default...
   ;; (setopt ef-themes-mixed-fonts t
   ;;         ef-themes-variable-pitch-ui t)
