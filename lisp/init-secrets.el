29d0e433c0c46c24426d195bd9f3973d4277d6a2
Chris Montgomery <chris@cdom.io>
2024-02-26 11:45:00 -0500

    feat(secrets): add configuration and helpers for auth-source-pass

--- lisp/init-secrets.el
+++ lisp/init-secrets.el
@@ -20,20 +20,67 @@
 
 ;;; Commentary:
 
-;; Examples may or may not include pinentry, handling file encryption,
-;; password-store integration, etc.
+;; GnuPG, pinentry, `auth-source', Unix password store, etc.
+
+;;; Sources:
+
+;; <https://github.com/jwiegley/dot-emacs/blob/9d595c427136e2709dee33271db1a658493265bd/init.org#auth-source-pass>
 
 ;;; Code:
 
 (require 'epg)
+(require 'auth-source)
+(require 'auth-source-pass)
+
+(require 'ceamx-paths)
+(require 'lib-common)
+
+;;; Configure secrets lookup with `auth-source' and the Unix password store
+
 ;; Ensure secrets and auth credentials are not stored in plaintext (the default).
 ;;
 ;; It's best to list only a single file here to avoid confusion about where
 ;; secrets might be stored.
 (setopt auth-sources (list "~/.authinfo.gpg"))
 
+(use-feature! auth-source-pass
+  :preface
+  (defvar +auth-source-pass--cache (make-hash-table :test #'equal))
 
-(setopt epg-pinentry-mode 'loopback)
+  (defun +auth-source-pass--reset-cache ()
+    (setq +auth-source-pass--cache (make-hash-table :test #'equal)))
+
+  (defun +auth-source-pass--read-entry (entry)
+    "Return a string with the file content of ENTRY."
+    (run-at-time 45 nil #'+auth-source-pass--reset-cache)
+    (let ((cached (gethash entry +auth-source-pass--cache)))
+      (or cached
+        (puthash
+          entry
+          (with-temp-buffer
+            (insert-file-contents (expand-file-name
+                                    (format "%s.gpg" entry)
+                                    (getenv "PASSWORD_STORE_DIR")))
+            (buffer-substring-no-properties (point-min) (point-max)))
+          +auth-source-pass--cache))))
+
+  (defun ceamx-auth-source-pass-list-items ()
+    "Return a list of all password store items."
+    (let ((store-dir (getenv "PASSWORD_STORE_DIR")))
+      (mapcar
+        (lambda (file)
+          (file-name-sans-extension (file-relative-name file store-dir)))
+        (directory-files-recursively store-dir "\.gpg$"))))
+
+  :config
+  (auth-source-pass-enable))
+
+;;; Use Emacs for pinentry
+
+(use-feature! epg
+  :defer 2
+  :config
+  (setopt epg-pinentry-mode 'loopback))
 
 (provide 'init-secrets)
 ;;; init-secrets.el ends here
