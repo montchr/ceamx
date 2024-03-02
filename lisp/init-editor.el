85c02f991a48ebebaff4aa4c0cdbce8dc9f2303a
Chris Montgomery <chris@cdom.io>
2024-03-01 21:56:06 -0500

    feat(editor): add multiple-cursors

--- lisp/init-editor.el
+++ lisp/init-editor.el
@@ -260,5 +260,47 @@ _h_   _l_     _y_ank        _t_ype       _e_xchange-point          /,`.-'`'   ..
       ("u" undo nil)
       ("g" nil))))
 
+;;; Add support for Sublime-like multi-cursor editing with `multiple-cursors'
+
+;; <https://github.com/magnars/multiple-cursors.el>
+
+(use-package multiple-cursors
+  :demand t
+  ;; :autoload (mc/num-cursors)
+
+  :config
+
+  (use-feature! hydra
+    :config
+
+    ;; TODO: convert to transient
+    (defhydra hydra-multiple-cursors (:hint nil)
+      "
+ Up^^             Down^^           Miscellaneous           % 2(mc/num-cursors) cursor%s(if (> (mc/num-cursors) 1) \"s\" \"\")
+------------------------------------------------------------------
+ [_p_]   Next     [_n_]   Next     [_l_] Edit lines  [_0_] Insert numbers
+ [_P_]   Skip     [_N_]   Skip     [_a_] Mark all    [_A_] Insert letters
+ [_M-p_] Unmark   [_M-n_] Unmark   [_s_] Search      [_q_] Quit
+ [_|_] Align with input CHAR       [Click] Cursor at point"
+      ("l" mc/edit-lines :exit t)
+      ("a" mc/mark-all-like-this :exit t)
+      ("n" mc/mark-next-like-this)
+      ("N" mc/skip-to-next-like-this)
+      ("M-n" mc/unmark-next-like-this)
+      ("p" mc/mark-previous-like-this)
+      ("P" mc/skip-to-previous-like-this)
+      ("M-p" mc/unmark-previous-like-this)
+      ("|" mc/vertical-align)
+      ("s" mc/mark-all-in-region-regexp :exit t)
+      ("0" mc/insert-numbers :exit t)
+      ("A" mc/insert-letters :exit t)
+      ("<mouse-1>" mc/add-cursor-on-click)
+      ;; Help with click recognition in this hydra
+      ("<down-mouse-1>" ignore)
+      ("<drag-mouse-1>" ignore)
+      ("q" nil))))
+
+
+
 (provide 'init-editor)
 ;;; init-editor.el ends here
