e434834178eb6dde18a05717d91cf8dd2ab6e7d5
Chris Montgomery <chris@cdom.io>
2024-03-07 16:49:44 -0500

    refactor(env): replace use-package usages

--- lisp/init-env.el
+++ lisp/init-env.el
@@ -27,7 +27,11 @@
 
 ;;; Code:
 
+(require 'elpaca-autoloads)
+
 (require 'config-env)
+
+(require 'lib-common)
 
 ;; Disable unnecessary OS-specific command-line options.
 (unless +sys-mac-p
@@ -35,41 +39,37 @@
 (unless +sys-linux-p
   (setq command-line-x-option-alist nil))
 
-(use-package exec-path-from-shell
-  :demand t
-  :commands (exec-path-from-shell-initialize)
-  :defines (exec-path-from-shell-variables)
-
-  :config
+(elpaca exec-path-from-shell
+  (require 'exec-path-from-shell)
   (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE" "NIX_SSL_CERT_FILE" "NIX_PATH"))
     (add-to-list 'exec-path-from-shell-variables var))
   (exec-path-from-shell-initialize))
 
-;;; inheritenv
-;;  Make temp buffers inherit buffer-local environment variables.
-;;  <https://github.com/purcell/inheritenv>
-(use-package inheritenv
-  :demand t
-  :after (exec-path-from-shell))
+;;; Make temporary buffers inherit buffer-local environment variables with `inheritenv'
 
-(use-package envrc
-  :after (exec-path-from-shell)
-  ;; FIXME: because exec-path-from-shell doesn't start reliably, this never happens
-  ;; :when (executable-find "direnv")
-  :commands (envrc-global-mode)
-  :init
-  (envrc-global-mode)
-  :config
-  ;; <https://github.com/doomemacs/doomemacs/blob/e96624926d724aff98e862221422cd7124a99c19/modules/tools/direnv/config.el#L11-L24>
-  (defun +direnv-init-global-mode-earlier-h ()
-    (let ((fn #'envrc-global-mode-enable-in-buffers))
-      (if (not envrc-global-mode)
-          (remove-hook 'change-major-mode-after-body-hook fn)
-        (remove-hook 'after-change-major-mode-hook fn)
-        (add-hook 'change-major-mode-after-body-hook fn 100))))
-  ;; TODO: remove or re-enable
-  ;;(add-hook 'envrc-global-mode-hook #'+direnv-init-global-mode-earlier-h)
-  )
+;; <https://github.com/purcell/inheritenv>
+
+(after! 'exec-path-from-shell
+  (elpaca inheritenv
+    (require 'inheritenv)))
+
+;;; Support integration with Direnv via the `envrc' package
+
+;; <https://github.com/purcell/envrc>
+;; <https://direnv.net/>
+;; <https://github.com/direnv/direnv>
+
+;; > Q: How does this differ from `direnv.el`?
+;;
+;; > <https://github.com/wbolster/emacs-direnv> repeatedly changes the global
+;; > Emacs environment, based on tracking what buffer you're working on.
+;;
+;; > Instead, `envrc.el` simply sets and stores the right environment in each
+;; > buffer, as a buffer-local variable.
+
+(after! 'exec-path-from-shell
+  (elpaca envrc
+    (envrc-global-mode)))
 
 (elpaca-wait)
 
