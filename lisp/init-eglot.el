10d20a678a128364fe4cefac9bd2dd13657d9b45
Chris Montgomery <chris@cdom.io>
2024-03-11 17:04:40 -0400

    feat(eglot|consult|flycheck): install integrations

--- lisp/init-eglot.el
+++ lisp/init-eglot.el
@@ -27,80 +27,59 @@
 
 ;;; Code:
 
+;;; Requirements
+
+(require 'ceamx-keymaps)
+
 (require 'lib-common)
+(require 'lib-keys)
 
-(use-feature! eglot
-  :after (jsonrpc)
-  :commands (eglot eglot-ensure)
+(autoload-macro! 'elpaca "elpaca")
 
-  :init
+;;; Configure
+
+(setopt eglot-sync-connect 1)
+(setopt eglot-autoshutdown t)
+(setopt eglot-send-changes-idle-time 0.5)
+
+;; Disable events buffer, which poses performance issues over time as the
+;; buffer grows in a longer-running Emacs instance.
+(setopt eglot-events-buffer-size 0)
+
+;; Prevent frequent focus-stealing.
+(setopt eglot-auto-display-help-buffer nil)
+
+(keys! ceamx-code-map
+  "a" '("action.." . eglot-code-actions)
+  "r" '("rename..." . eglot-rename))
+
+(after! 'eglot
+  (defvar eglot-server-programs)
+
   (def-advice! +eglot--ensure-available-mode (fn)
     :around #'eglot-ensure
     "Run `eglot-ensure' in supported modes."
     (when (alist-get major-mode eglot-server-programs nil nil
-                     (lambda (modes key)
-                       (if (listp modes)
-                           (member key modes)
-                         (eq key modes))))
+            (lambda (modes key)
+              (if (listp modes)
+                (member key modes)
+                (eq key modes))))
       (funcall fn)))
 
-  (setopt eglot-sync-connect 1)
-  (setopt eglot-autoshutdown t)
-  (setopt eglot-send-changes-idle-time 0.5)
-
-  ;; Disable events buffer, which poses performance issues over time as the
-  ;; buffer grows in a longer-running Emacs instance.
-  (setopt eglot-events-buffer-size 0)
-
-  ;; Prevent frequent focus-stealing.
-  (setopt eglot-auto-display-help-buffer nil)
-
-  :config
   ;; Register eglot help buffers as popup windows.
   (after! [popper]
     (defvar popper-reference-buffers)
-    ;; TODO: make this a macro?
-    (setopt popper-reference-buffers
-            (append popper-reference-buffers '("^\\*eglot-help")))))
+    (add-to-list 'popper-reference-buffers "^\\*eglot-help")))
 
+;;; Configure Flycheck integration
 
-;; TODO: <https://github.com/doomemacs/doomemacs/blob/master/modules/tools/lsp/%2Beglot.el>
-;; (def-advice! +lsp--defer-server-shutdown-a (fn &optional server)
-;;   :around #'eglot--managed-mode
-;;   "Defer server shutdown for a few seconds.
-;; This gives the user a chance to open other project files before the server is
-;; auto-killed (which is a potentially expensive process). It also prevents the
-;; server getting expensively restarted when reverting buffers."
-;;   (letf! (defun eglot-shutdown (server)
-;;            (if (or (null +lsp-defer-shutdown)
-;;                    (eq +lsp-defer-shutdown 0))
-;;                (prog1 (funcall eglot-shutdown server)
-;;                  (+lsp-optimization-mode -1))
-;;              (run-at-time
-;;               (if (numberp +lsp-defer-shutdown) +lsp-defer-shutdown 3)
-;;               nil (lambda (server)
-;;                     (unless (eglot--managed-buffers server)
-;;                       (prog1 (funcall eglot-shutdown server)
-;;                         (+lsp-optimization-mode -1))))
-;;               server)))
-;;          (funcall fn server)))
-
-
-
-
-;; FIXME: install package
-
-;; (use-package flycheck-eglot
-;;   :after (flycheck eglot)
-;;   :init
-;;   (add-hook 'eglot-managed-mode-hook #'flycheck-eglot-mode))
+(elpaca flycheck-eglot
+  (add-hook 'eglot-managed-mode-hook #'flycheck-eglot-mode))
 
 ;;; `consult-eglot' :: <https://github.com/mohkale/consult-eglot>
 
-;; FIXME: install package
-
-;; (use-package consult-eglot
-;;   :after (consult eglot))
+(elpaca consult-eglot
+  (defalias 'ceamx/list-workspace-symbols #'consult-eglot-symbols))
 
 (provide 'init-eglot)
 ;;; init-eglot.el ends here
