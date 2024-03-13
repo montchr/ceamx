7006d6d6b1edbfb6dce6f07a59e9287a956df0e8
Chris Montgomery <chris@cdom.io>
2024-03-12 21:14:27 -0400

    fix(themes:ef|font): tune heading face properties + allow mixed pitch

--- lisp/init-ui-ef-themes.el
+++ lisp/init-ui-ef-themes.el
@@ -36,28 +36,22 @@
   :commands (ef-themes-select)
 
   :config
-  (setopt ceamx-theme-default-dark 'ef-trio-dark)
+  (setopt ceamx-theme-default-dark 'ef-night)
   (setopt ceamx-theme-default-light 'ef-frost)
 
   (setopt ef-themes-to-toggle (list ceamx-theme-default-dark ceamx-theme-default-light))
 
-  (setopt ef-themes-headings nil)
-  ;; (setopt ef-themes-headings         ; read the manual's entry or the doc string
-  ;;   '((0 variable-pitch light 1.9)
-  ;;      (1 variable-pitch light 1.8)
-  ;;      (2 variable-pitch regular 1.7)
-  ;;      (3 variable-pitch regular 1.6)
-  ;;      (4 variable-pitch regular 1.5)
-  ;;      (5 variable-pitch 1.4)           ; absence of weight means `bold'
-  ;;      (6 variable-pitch 1.3)
-  ;;      (7 variable-pitch 1.2)
-  ;;      (t variable-pitch 1.1)))
-  ;; They are nil by default...
-  ;; (setopt ef-themes-mixed-fonts t
-  ;;         ef-themes-variable-pitch-ui t)
+  (setopt ef-themes-headings
+    '(;;(0 variable-pitch medium 1.7)
+       (1 variable-pitch 1.5)
+       (2 variable-pitch 1.3)
+;;       (3 variable-pitch 1.1)
+       (t variable-pitch 1.1)))
 
-  ;; Disable all other themes to avoid awkward blending:
-  ;; TODO: how does this help when using theme-buffet-mode
+  (setopt ef-themes-mixed-fonts t
+          ef-themes-variable-pitch-ui nil)
+
+  ;; Disable all other themes to avoid awkward blending
   (mapc #'disable-theme custom-enabled-themes)
 
   ;; The themes we provide are recorded in the `ef-themes-dark-themes',
