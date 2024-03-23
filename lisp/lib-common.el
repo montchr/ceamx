5dd4342b76e37b480f27d94a093e99c36ba43135
Chris Montgomery <chris@cdom.io>
2024-03-23 13:56:09 -0400

    lib: tangle

--- lisp/lib-common.el
+++ lisp/lib-common.el
@@ -71,44 +71,58 @@
 ;; Avoid any library dependencies which are not already part of Emacs.  This
 ;; library will be loaded before packages are available.
 
-;;; Sources:
-
-;; <https://github.com/doomemacs/doomemacs/blob/03d692f129633e3bf0bd100d91b3ebf3f77db6d1/lisp/doom-lib.el>
-;; <https://github.com/radian-software/radian/blob/9a82b6e7395b3f1f143b91f8fe129adf4ef31dc7/emacs/radian.el>
-;; <https://github.com/doomemacs/doomemacs/blob/986398504d09e585c7d1a8d73a6394024fe6f164/lisp/doom-keybinds.el#L93C1-L109C56>
-;; <https://github.com/casouri/lunarymacs/blob/cd1f34449038e5ec371b1277941c529ea1fb4e9e/site-lisp/luna-key.el>
-
 ;;; Code:
 
-;;; Requirements
+;;;; Requirements
 
 (require 'cl-lib)
 (require 'map)
 (require 'seq)
 
-;;; General
+;; ~ceamx-host-p~: Determine whether Emacs is running on a given host
 
+
+;; [[file:../config.org::*~ceamx-host-p~: Determine whether Emacs is running on a given host][~ceamx-host-p~: Determine whether Emacs is running on a given host:1]]
 (defun ceamx-host-p (name)
   "Whether Emacs is running on the machine NAME."
   (string= name (system-name)))
+;; ~ceamx-host-p~: Determine whether Emacs is running on a given host:1 ends here
 
+;; ~ceamx-unquote~: Unquote an Elisp expression
+
+
+;; [[file:../config.org::*~ceamx-unquote~: Unquote an Elisp expression][~ceamx-unquote~: Unquote an Elisp expression:1]]
 (defun ceamx-unquote (exp)
   "Return EXP unquoted."
   (declare (pure t) (side-effect-free t))
   (while (memq (car-safe exp) '(quote function))
     (setq exp (cadr exp)))
   exp)
+;; ~ceamx-unquote~: Unquote an Elisp expression:1 ends here
 
-;; via <https://protesilaos.com/emacs/dotemacs#h:3563ceb5-b70c-4191-9c81-f2f5a202c4da>
+;; ~noop!~: Make the wrapped expression do nothing
+
+;; - source :: <https://protesilaos.com/emacs/dotemacs#h:3563ceb5-b70c-4191-9c81-f2f5a202c4da>
+
+
+;; [[file:../config.org::*~noop!~: Make the wrapped expression do nothing][~noop!~: Make the wrapped expression do nothing:1]]
 (defmacro noop! (&rest _body)
   "Do nothing with BODY and return nil.
 Unlike `ignore', produce no side effects."
   (declare (indent defun))
   nil)
+;; ~noop!~: Make the wrapped expression do nothing:1 ends here
 
-;;; Loading and Evalling
+;; ~after!~: Evaluate an expression after the given feature(s) have loaded
 
-;; via <https://github.com/bling/dotemacs/blob/97c72c8425c5fb40ca328d1a711822ce0a0cfa26/core/core-boot.el#L53-L74>
+;; - source :: <https://github.com/bling/dotemacs/blob/97c72c8425c5fb40ca328d1a711822ce0a0cfa26/core/core-boot.el#L53-L74>
+
+;; - [ ] Doom's implementation is more flexible and handles undefined symbols, but
+;;   does not support quoted FEATURE. Some new macro supporting quoted FEATURE and
+;;   the features of Doom's version would probably be ideal.
+
+
+;; [[file:../config.org::*~after!~: Evaluate an expression after the given feature(s) have loaded][~after!~: Evaluate an expression after the given feature(s) have loaded:1]]
 (defmacro after! (feature &rest body)
   "Execute BODY after FEATURE has been loaded.
 
@@ -117,12 +131,7 @@ FEATURE may be any one of:
     \"evil-autoloads\" => (with-eval-after-load \"evil-autoloads\" BODY)
     [evil cider]     => (with-eval-after-load \\='evil
                           (with-eval-after-load \\='cider
-                            BODY))
-
-TODO: Doom's implementation is more flexible and handles
-undefined symbols, but does not support quoted FEATURE. Some new
-macro supporting quoted FEATURE and the features of Doom's
-version would probably be ideal."
+                            BODY))"
   (declare (indent 1))
   (cond
    ((vectorp feature)
@@ -135,8 +144,14 @@ version would probably be ideal."
       prog))
    (t
     `(with-eval-after-load ,feature ,@body))))
+;; ~after!~: Evaluate an expression after the given feature(s) have loaded:1 ends here
 
-;; via <https://github.com/bling/dotemacs/blob/97c72c8425c5fb40ca328d1a711822ce0a0cfa26/core/core-boot.el#L83C1-L88C25>
+;; ~defer!~: Evaluate an expression after Emacs is idle for some time
+
+;; - source :: <https://github.com/bling/dotemacs/blob/97c72c8425c5fb40ca328d1a711822ce0a0cfa26/core/core-boot.el#L83C1-L88C25>
+
+
+;; [[file:../config.org::*~defer!~: Evaluate an expression after Emacs is idle for some time][~defer!~: Evaluate an expression after Emacs is idle for some time:1]]
 (defmacro defer! (secs &rest body)
   "Run BODY when Emacs is idle for SECS seconds."
   (declare (indent defun) (debug t))
@@ -144,8 +159,14 @@ version would probably be ideal."
     ,secs
     nil
     (lambda () ,@body)))
+;; ~defer!~: Evaluate an expression after Emacs is idle for some time:1 ends here
 
-;; via <https://github.com/doomemacs/doomemacs/blob/03d692f129633e3bf0bd100d91b3ebf3f77db6d1/lisp/doom-lib.el#L686-L701>
+;; ~defer-until!~: Evaluate an expression when a condition is non-nil
+
+;; - source :: <https://github.com/doomemacs/doomemacs/blob/03d692f129633e3bf0bd100d91b3ebf3f77db6d1/lisp/doom-lib.el#L686-L701>
+
+
+;; [[file:../config.org::*~defer-until!~: Evaluate an expression when a condition is non-nil][~defer-until!~: Evaluate an expression when a condition is non-nil:1]]
 (defmacro defer-until! (condition &rest body)
   "Run BODY when CONDITION is non-nil.
 Leverages checks via `after-load-functions'.
@@ -163,37 +184,50 @@ Meant to serve as a predicated alternative to `after!'."
                         ,@body)))
           (put ',fn 'permanent-local-hook t)
           (add-hook 'after-load-functions #',fn)))))
+;; ~defer-until!~: Evaluate an expression when a condition is non-nil:1 ends here
 
-(defmacro autoload-macro! (macro file)
-  "Define MACRO to autoload from FILE.
-MACRO and FILE are as in `autoload', which see.
+;; ~appendq!~: Append lists to a symbol in place
 
-This provides `autoload' with nil values for its DESCRIPTION and
-INTERACTIVE arguments. Its TYPE argument is non-nil, indicating
-that the symbol to be autoload is actually a macro."
-  (declare (debug t))
-  `(autoload ,macro ,file nil nil t))
 
-;;; Variables
-
+;; [[file:../config.org::*~appendq!~: Append lists to a symbol in place][~appendq!~: Append lists to a symbol in place:1]]
 (defmacro appendq! (sym &rest lists)
   "Append LISTS to SYM in place."
   `(setq ,sym (append ,sym ,@lists)))
+;; ~appendq!~: Append lists to a symbol in place:1 ends here
 
+;; ~prependq!~: Prepend lists to a symbol in place
+
+
+;; [[file:../config.org::*~prependq!~: Prepend lists to a symbol in place][~prependq!~: Prepend lists to a symbol in place:1]]
 (defmacro prependq! (sym &rest lists)
   "Prepend LISTS to SYM in place."
   `(setq ,sym (append ,@lists ,sym)))
+;; ~prependq!~: Prepend lists to a symbol in place:1 ends here
 
+;; ~appendopt!~ Append lists to an existing user option
+
+
+;; [[file:../config.org::*~appendopt!~ Append lists to an existing user option][~appendopt!~ Append lists to an existing user option:1]]
 (defmacro appendopt! (variable &rest lists)
   "Append LISTS to the existing user option VARIABLE.
 This uses `setopt' to set the new value of VARIABLE."
   `(setopt ,variable (append ,variable ,@lists)))
+;; ~appendopt!~ Append lists to an existing user option:1 ends here
 
+;; ~prependopt!~: Prepend lists to an existing user option
+
+
+;; [[file:../config.org::*~prependopt!~: Prepend lists to an existing user option][~prependopt!~: Prepend lists to an existing user option:1]]
 (defmacro prependopt! (variable &rest lists)
   "Prepend LISTS to the existing user option VARIABLE.
 This uses `setopt' to set the new value of VARIABLE."
   `(setopt ,variable (append ,@lists ,variable)))
+;; ~prependopt!~: Prepend lists to an existing user option:1 ends here
 
+;; ~delq!~: Delete an element from a list in-place
+
+
+;; [[file:../config.org::*~delq!~: Delete an element from a list in-place][~delq!~: Delete an element from a list in-place:1]]
 (defmacro delq! (elt list &optional fetcher)
   "`delq' ELT from LIST in-place.
 If FETCHER is a function, ELT is used as the key in LIST (an alist)."
@@ -201,7 +235,12 @@ If FETCHER is a function, ELT is used as the key in LIST (an alist)."
                           `(funcall ,fetcher ,elt ,list)
                         elt)
                 ,list)))
+;; ~delq!~: Delete an element from a list in-place:1 ends here
 
+;; ~pushnew!~: Push values sequentially into a list uniquely
+
+
+;; [[file:../config.org::*~pushnew!~: Push values sequentially into a list uniquely][~pushnew!~: Push values sequentially into a list uniquely:1]]
 ;; TODO: another version to test car of alist so that new additions with the
 ;;       same car will override the existing list
 (defmacro pushnew! (place &rest values)
@@ -210,19 +249,24 @@ This is a variadic `cl-pushnew'."
   (let ((var (make-symbol "result")))
     `(dolist (,var (list ,@values) (with-no-warnings ,place))
       (cl-pushnew ,var ,place :test #'equal))))
+;; ~pushnew!~: Push values sequentially into a list uniquely:1 ends here
 
-;;; Filesystem
+;; ~ceamx-subdirs~: List every non-hidden subdirectory of a parent directory
 
+
+;; [[file:../config.org::*~ceamx-subdirs~: List every non-hidden subdirectory of a parent directory][~ceamx-subdirs~: List every non-hidden subdirectory of a parent directory:1]]
 (defun ceamx-subdirs (parent-dir)
   "Return every non-hidden subdirectory of PARENT-DIR."
   (cl-remove-if-not
    #'file-directory-p
    (directory-files
     (expand-file-name parent-dir) t "^[^\\.]")))
+;; ~ceamx-subdirs~: List every non-hidden subdirectory of a parent directory:1 ends here
+
+;; ~def-advice!~: Macro to define and add advice to a function
 
 
-;;; Advice
-
+;; [[file:../config.org::*~def-advice!~: Macro to define and add advice to a function][~def-advice!~: Macro to define and add advice to a function:1]]
 (defmacro def-advice! (name arglist how symbol docstring &rest body)
   "Define an advice called NAME and add it to a function.
 ARGLIST, DOCSTRING, and BODY are as in `defun'.
@@ -254,9 +298,12 @@ the newly-defined advice.  SYMBOL is the function to be advised."
        (declare-function ,name nil))
      (advice-add ,symbol ',how #',name)
      ',name))
+;; ~def-advice!~: Macro to define and add advice to a function:1 ends here
 
-;;; Hooks
+;; ~def-hook!~: Macro to define a hook function and add it to the given hook(s)
 
+
+;; [[file:../config.org::*~def-hook!~: Macro to define a hook function and add it to the given hook(s)][~def-hook!~: Macro to define a hook function and add it to the given hook(s):1]]
 (defmacro def-hook! (name arglist hooks docstring &rest body)
   "Define function NAME and add it to HOOKS.
 ARGLIST is as in `defun'.  HOOKS is a list of hooks to which to
@@ -280,9 +327,12 @@ as in `defun'."
         ,@body)
        (dolist (hook ',hooks)
         (add-hook hook ',name)))))
+;; ~def-hook!~: Macro to define a hook function and add it to the given hook(s):1 ends here
 
-;;; Packages
+;; ~use-feature!~: Configuration-only wrapper for ~use-package~
 
+
+;; [[file:../config.org::*~use-feature!~: Configuration-only wrapper for ~use-package~][~use-feature!~: Configuration-only wrapper for ~use-package~:1]]
 (defmacro use-feature! (name &rest args)
   "Configuration-only wrapper for `use-package', passing through NAME and ARGS.
 
@@ -296,7 +346,14 @@ will be nil."
   `(use-package ,name
      :ensure nil
      ,@args))
+;; ~use-feature!~: Configuration-only wrapper for ~use-package~:1 ends here
 
+;; ~package!~: Declare a package and its initial configuration
+
+;; Wrapper for ~elpaca~ to avoid having to declare its autoloads in every file.
+
+
+;; [[file:../config.org::*~package!~: Declare a package and its initial configuration][~package!~: Declare a package and its initial configuration:1]]
 (defmacro package! (order &rest body)
   "Declare a package ORDER and its initial configuration BODY.
 Provides the necessary autoloads so that we can declare packages
@@ -305,6 +362,7 @@ without needing to declare autoloads for `elpaca' in every file."
   `(progn
      (autoload 'elpaca "elpaca" nil nil t)
      (elpaca ,order ,@body)))
+;; ~package!~: Declare a package and its initial configuration:1 ends here
 
 ;; ~ceamx-normalize-char~ :: Angry wrapper around ~string-to-char~
 
@@ -336,36 +394,26 @@ vary based on... various reasons?"
             (t
              (string-to-char char)))
     char))
+;; ~ceamx-normalize-char~ :: Angry wrapper around ~string-to-char~:1 ends here
 
+;; ~global-keys!~: Define multiple global keybindings
+
+
+;; [[file:../config.org::*~global-keys!~: Define multiple global keybindings][~global-keys!~: Define multiple global keybindings:1]]
 (defmacro global-keys! (&rest keys)
   "Define keybindings KEYS in the global keymap.
 Wrapper for `define-keymap' with `current-global-map' as target keymap."
   (declare (indent defun) (debug t))
   `(define-keymap :keymap (current-global-map)
      ,@keys))
+;; ~global-keys!~: Define multiple global keybindings:1 ends here
 
-(defmacro leader-key! (key def)
-  "Bind DEF to KEY in the leader map.
-If `meow-leader-define-key' is available, then that function will
-handle the binding. Otherwise, binding will be handled with
-`keymap-set' into `mode-specific-map'.
-
-Note that as of writing, `meow' bindings do not seem to support
-descriptions in a way that is comprehensible to `which-key' --
-the documentation for `meow-keypad-describe-keymap-function'
-acknowledges this incompatibility."
-  (declare (indent defun))
-  `(if (fboundp 'meow-leader-define-key)
-       (meow-leader-define-key (cons ,key ,def))
-     (keymap-set mode-specific-map ,key ,def)))
-;; Input and Keybindings:1 ends here
-
-;; A function to convert a regular keymap to a repeat-map
+;; ~ceamx-repeatify-keymap~: convert a regular keymap to a repeat-map
 
 ;; [[https://old.reddit.com/r/emacs/comments/1adwnse/repeatmode_is_awesome_share_you_useful_configs/kk9vpif/][oantolin comments on Repeat-mode is awesome, share you useful configs]]
 
 
-;; [[file:../config.org::*A function to convert a regular keymap to a repeat-map][A function to convert a regular keymap to a repeat-map:1]]
+;; [[file:../config.org::*~ceamx-repeatify-keymap~: convert a regular keymap to a repeat-map][~ceamx-repeatify-keymap~: convert a regular keymap to a repeat-map:1]]
 (defun ceamx-repeatify-keymap (repeat-map)
   "Set the `repeat-map' property on all commands bound in REPEAT-MAP."
   (named-let process ((keymap (symbol-value repeat-map)))
@@ -375,7 +423,7 @@ acknowledges this incompatibility."
         ((symbolp cmd) (put cmd 'repeat-map repeat-map))
         ((keymapp cmd) (process cmd))))
      keymap)))
-;; A function to convert a regular keymap to a repeat-map:1 ends here
+;; ~ceamx-repeatify-keymap~: convert a regular keymap to a repeat-map:1 ends here
 
 (provide 'lib-common)
 ;;; lib-common.el ends here
