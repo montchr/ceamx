c0cffa460a1475a5b9b1b98667a4779121e93e0b
Chris Montgomery <chris@cdom.io>
2024-04-09 21:50:20 -0400

    theme:modus: use-package-less

--- lisp/init-ui-modus-themes.el
+++ lisp/init-ui-modus-themes.el
@@ -26,47 +26,42 @@
 
 (require 'lib-common)
 
-(use-package modus-themes
-  :ensure t
-  :demand t
-  :config
-  ;; Note that these two settings do not necessarily disable italic/bold
-  ;; entirely -- they only control whether those styles will be used in more or
-  ;; less contexts.
+(package! modus-themes
+  (require 'modus-themes)
+
   (setopt modus-themes-italic-constructs t)
   (setopt modus-themes-bold-constructs nil)
-
   (setopt modus-themes-mixed-fonts t)
   (setopt modus-themes-variable-pitch-ui nil)
   (setopt modus-themes-disable-other-themes t)
   (setopt modus-themes-to-toggle '(modus-operandi-tinted modus-vivendi-tinted))
 
   (setopt modus-themes-common-palette-overrides
-    '(
-       ;; Make the fringe invisible.
-       (fringe unspecified)
-       ;; Make line numbers less intense and add a shade of cyan
-       ;; for the current line number.
-       (fg-line-number-inactive "gray50")
-       (fg-line-number-active cyan-cooler)
-       (bg-line-number-inactive unspecified)
-       (bg-line-number-active unspecifed)))
+          '(
+            ;; Make the fringe invisible.
+            (fringe unspecified)
+            ;; Make line numbers less intense and add a shade of cyan
+            ;; for the current line number.
+            (fg-line-number-inactive "gray50")
+            (fg-line-number-active cyan-cooler)
+            (bg-line-number-inactive unspecified)
+            (bg-line-number-active unspecifed)))
 
   (def-hook! +modus-themes-custom-faces-h ()
     'modus-themes-after-load-theme-hook
     "Configurate custom faces for the `modus-themes'."
     (modus-themes-with-colors
       (custom-set-faces
-        ;; Add "padding" to the mode lines.
-        `(mode-line ((,c :box (:line-width 10
-                                :color ,bg-mode-line-active))))
-        `(mode-line-inactive ((,c :box (:line-width 10
-                                         :color ,bg-mode-line-inactive)))))))
+       ;; Add "padding" to the mode lines.
+       `(mode-line ((,c :box (:line-width 10
+                              :color ,bg-mode-line-active))))
+       `(mode-line-inactive ((,c :box (:line-width 10
+                                       :color ,bg-mode-line-inactive)))))))
 
   ;; Do not extend `region' background past the end of the line.
   ;; <https://protesilaos.com/emacs/modus-themes#h:a5140c9c-18b2-45db-8021-38d0b5074116>
   (custom-set-faces
-    '(region ((t :extend nil))))
+   '(region ((t :extend nil))))
 
   ;; NOTE: Loaded in `init-ui-circadian'.
   ;; (load-theme 'modus-vivendi-tinted :no-confirm)
