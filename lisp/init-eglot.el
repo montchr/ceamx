d5ab686c69319d79017744759d15cecaeb531316
Chris Montgomery <chris@cdom.io>
2024-04-30 22:03:30 -0400

    eglot: tangled

--- lisp/init-eglot.el
+++ lisp/init-eglot.el
@@ -1,40 +1,31 @@
-;;; init-eglot.el --- Eglot support                  -*- lexical-binding: t; -*-
+;;; init-eglot.el --- Eglot support  -*- lexical-binding: t;  -*-
 
-;; Copyright (C) 2023-2024  Chris Montgomery
+;; Copyright (c) 2023-2024  Chris Montgomery <chris@cdom.io>
 
 ;; Author: Chris Montgomery <chris@cdom.io>
-;; Keywords: local
+;; URL: https://git.sr.ht/~montchr/ceamx
+;; Version: 0.1.0
 
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
+;; This file is NOT part of GNU Emacs.
 
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
+;; This file is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the
+;; Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; This file is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
 ;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+;; along with this file.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-
-;; Configure Eglot.
-
-;; TODO: Consider implementing Doom's approach to handling performance.
-;; <https://github.com/doomemacs/doomemacs/blob/03d692f129633e3bf0bd100d91b3ebf3f77db6d1/modules/tools/lsp/config.el>
-
 ;;; Code:
 
-;;; Requirements
-
 (require 'ceamx-keymaps)
-
 (require 'lib-common)
-
-;;; Configure
-
 (setopt eglot-sync-connect 1)
 (setopt eglot-autoshutdown t)
 (setopt eglot-send-changes-idle-time 0.5)
@@ -49,7 +40,6 @@
 (define-keymap :keymap ceamx-code-map
   "a" '("action.." . eglot-code-actions)
   "r" '("rename..." . eglot-rename))
-
 (after! eglot
   (defvar eglot-server-programs)
 
@@ -57,24 +47,16 @@
     :around #'eglot-ensure
     "Run `eglot-ensure' in supported modes."
     (when (alist-get major-mode eglot-server-programs nil nil
-            (lambda (modes key)
-              (if (listp modes)
-                (member key modes)
-                (eq key modes))))
-      (funcall fn)))
-
-  ;; Register eglot help buffers as popup windows.
-  (after! popper
-    (defvar popper-reference-buffers)
-    (add-to-list 'popper-reference-buffers "^\\*eglot-help")))
-
-;;; Configure Flycheck integration
-
+                     (lambda (modes key)
+                       (if (listp modes)
+                           (member key modes)
+                         (eq key modes))))
+      (funcall fn))))
+(after! (eglot popper)
+  (defvar popper-reference-buffers)
+  (add-to-list 'popper-reference-buffers "^\\*eglot-help"))
 (package! flycheck-eglot
   (add-hook 'eglot-managed-mode-hook #'flycheck-eglot-mode))
-
-;;; ~consult-eglot~ :: <https://github.com/mohkale/consult-eglot>
-
 (package! consult-eglot
   (defalias 'ceamx/list-workspace-symbols #'consult-eglot-symbols))
 
