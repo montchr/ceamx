c0935f4c3da2142944e167e997471779ba729dc1
Chris Montgomery <chris@cdom.io>
2024-05-17 23:43:47 -0400

    config: move `org-archive-location` to dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,5 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((emacs-lisp-mode . ((flycheck-disabled-checkers . (emacs-lisp-checkdoc))))
- (org-mode . ((tab-width . 8))))
+ (org-mode . ((org-archive-location . graveyard/%s::datetree/)
+              (tab-width . 8))))
