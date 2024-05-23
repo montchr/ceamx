3f65429f34929e17f637ffff0eb401795927756a
Chris Montgomery <chris@cdom.io>
2024-05-23 17:12:17 -0400

    config: fixes to dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,5 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((emacs-lisp-mode . ((flycheck-disabled-checkers . (emacs-lisp-checkdoc))))
- (org-mode . ((org-archive-location . graveyard/%s::datetree/)
+((org-mode . ((org-archive-location . "graveyard/%s.archive::datetree/")
               (tab-width . 8))))
--- custom.el
+++ custom.el
@@ -4,19 +4,19 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values
-    '((org-refile-targets
-        ("config.org" :maxlevel . 2)
-        (nil :maxlevel . 4))
+    '((org-archive-location . "graveyard/%s.archive::datetree/")
+       (org-refile-targets
+         ("config.org" :maxlevel . 2)
+         (nil :maxlevel . 4))
        (org-refile-targets
          (nil :maxlevel . 4))
        (eval load-file "./ceamx-dev-loader.el")
        (apheleia-formatter . alejandra)
        (apheleia-formatter . nixfmt)
-       (org-archive-location . graveyard/%s::datetree/)
        (lentic-init . lentic-orgel-org-init))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(region ((t :extend nil))))
+ )
