c56b6cf2eb5aac1f5f26d022d12479d88a208f12
Chris Montgomery <chris@cdom.io>
2024-06-08 17:33:07 -0400

    chore: fmt

--- custom.el
+++ custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(safe-local-variable-values
+  '(safe-local-variable-values
     '((magit-todos-exclude-globs ".git/" "lisp/*.el")
        (org-refile-targets ("config.org" :maxlevel . 2) (nil :maxlevel . 4))
        (org-refile-targets (nil :maxlevel . 4))
