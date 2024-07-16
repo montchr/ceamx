a0ea9dfe032d4c33553c4862d1d12f6a5585890a
Chris Montgomery <chmont@proton.me>
2024-07-16 00:03:18 -0400

    chore: update `safe-local-variable-values`

--- custom.el
+++ custom.el
@@ -4,7 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values
-    '((magit-todos-exclude-globs ".git/" "lisp/*.el")
+    '((eval add-hook 'after-save-hook #'org-gfm-export-to-markdown t t)
+       (org-modern-hide-stars) (org-modern-keyword) (org-modern-todo)
+       (org-pretty-entities) (magit-todos-exclude-globs ".git/" "lisp/*.el")
        (org-refile-targets ("config.org" :maxlevel . 2) (nil :maxlevel . 4))
        (org-refile-targets (nil :maxlevel . 4))
        (eval load-file "./ceamx-dev-loader.el") (apheleia-formatter . alejandra)
