62b58eafaff81737ab9653d6dc90ff8c6398d9fd
Chris Montgomery <chmont@proton.me>
2025-02-08 11:47:12 -0500

    news: configure elfeed

--- lisp/ceamx-init-news.el
+++ lisp/ceamx-init-news.el
@@ -3,18 +3,64 @@
 (require 'ceamx-paths)
 
 (package! elfeed
-  (keymap-set ceamx-launch-prefix "f" #'elfeed))
+  (keymap-set ceamx-launch-prefix "f" #'elfeed)
+
+  (after! elfeed
+    (require 'ceamx-news)
+
+    (make-directory elfeed-db-directory t)
+
+    (setopt elfeed-search-filter "@1-week-ago +unread")
+    (setopt elfeed-show-entry-switch #'pop-to-buffer
+            elfeed-show-entry-delete #'ceamx-news/delete-pane))
+
+  (after! popper
+    (add-to-list 'popper-reference-buffers "^\\*elfeed-entry")))
+
+(package! elfeed-goodies
+  (after! elfeed
+    (require 'elfeed-goodies)
+    (elfeed-goodies/setup))
+  ;; (after! elfeed-goodies
+  ;;   )
+  )
 
 (package! elfeed-org
-  (require 'elfeed-org)
+  ;; HACK: Must be set prior to feature load.
+  ;; <https://github.com/doomemacs/doomemacs/blob/2bc052425ca45a41532be0648ebd976d1bd2e6c1/modules/app/rss/config.el#L74-L75>
+  (setq rmh-elfeed-org-files
+        (list (file-name-concat ceamx-reading-feed-dir "index.org")))
 
-  ;; When `elfeed' starts, `elfeed-org' will read the configuration.
-  (elfeed-org)
+  (after! elfeed
+    (require 'elfeed-org)
+    (elfeed-org)))
 
-  (setopt rmh-elfeed-org-files
-          (list
-           (file-name-concat ceamx-reading-feed-dir "index.org")
-           (locate-user-emacs-file "feeds.org"))))
+(package! elfeed-tube
+  (after! elfeed
+    (require 'elfeed-tube)
+    (keymap-set elfeed-show-mode-map "F" #'elfeed-tube-fetch)
+    (keymap-set elfeed-show-mode-map "<remap> <save-buffer>" #'elfeed-tube-save)
+    (keymap-set elfeed-search-mode-map "F" #'elfeed-tube-fetch)
+    (keymap-set elfeed-search-mode-map "<remap> <save-buffer>" #'elfeed-tube-save))
+  (after! elfeed-tube
+    (setopt elfeed-tube-auto-save-p nil
+            elfeed-tube-auto-fetch-p t)
+    (elfeed-tube-setup)))
+
+(package! elfeed-tube-mpv
+  (after! elfeed
+    (keymap-set elfeed-show-mode-map "C-c C-f" #'elfeed-tube-mpv-follow-mode)
+    (keymap-set elfeed-show-mode-map "C-c C-w" #'elfeed-tube-mpv-where)))
+
+(package! elfeed-score
+  (after! elfeed
+    (elfeed-score-enable))
+  (after! elfeed-score
+    ;; <https://www.unwoundstack.com/doc/elfeed-score/curr>
+    (setopt elfeed-score-serde-score-file
+            (file-name-concat ceamx-reading-feed-dir "elfeed-scores.eld")))
+  (after! (elfeed elfeed-score)
+    (keymap-set elfeed-search-mode-map "=" elfeed-score-map)))
 
 (provide 'ceamx-init-news)
 ;;; ceamx-init-news.el ends here
---
+++ site-lisp/ceamx-news.el
@@ -0,0 +1,108 @@
+;;; ceamx-news.el --- Ceamx News helpers             -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2025  Chris Montgomery
+
+;; Author: Chris Montgomery <chmont@protonmail.com>
+;; Keywords: news, extensions, local
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Helpers for `elfeed' and the like.
+
+;;; Code:
+
+;;;; Variables
+
+;;;; Customization
+
+(defgroup ceamx-news nil
+  "Ceamx News"
+  :group 'ceamx)
+
+(defcustom ceamx-activity-name-prefix "thee:"
+  "Name prefix string for `activities-mode' activities defined by Ceamx."
+  :type 'string
+  :group 'ceamx)
+
+(defcustom ceamx-news-activity-name "news"
+  "Activity-Name name for reading the news.
+The name will be prefixed with the value of
+`ceamx-activity-name-prefix'."
+  :type 'string
+  :group 'ceamx-news)
+
+;;;; Functions
+
+;;;###autoload
+(defun ceamx-news-dead-feeds (&optional years)
+  "Return a list of feeds that have not updated in YEARS.
+When YEARS is nil, the threshold will be one year."
+  (let* ( (years (or years 1.0))
+          (living-feeds (make-hash-table :test 'equal))
+          (seconds (* years 365.0 24 60 60))
+          (threshold (- (float-time) seconds)))
+    (with-elfeed-db-visit (entry-feed)
+      (let ((date (elfeed-entry-date entry)))
+        (when (> date threshold)
+          (setf (gethash (elfeed-feed-url feed) living-feeds) t))))
+    (seq-filter (##gethash % living-feeds) (elfeed-feed-list))))
+
+;;;; Commands
+
+;;;###autoload
+(defun ceamx-news/open-entry (entry)
+  "Open the currently-selected entry in a buffer."
+  (interactive (list (elfeed-search-selected :ignore-region)))
+  (when (elfeed-entry-p entry)
+    (elfeed-untag entry 'unread)
+    (elfeed-search-update-entry entry)
+    (elfeed-show-entry entry)))
+
+;;;###autoload
+(defun ceamx-news/delete-pane ()
+  "Delete the split feed-reader pane."
+  (interactive)
+  (let* ( (buf (get-buffer "*elfeed-entry*"))
+          (window (get-buffer-window buf)))
+    (delete-window window)
+    (when (buffer-live-p buf)
+      (kill-buffer buf))))
+
+;;;; Minor-Modes
+
+
+
+;; FIXME: wip
+;; (defun ceamx/thee-news ()
+;;   (interactive)
+;;   (let* ( (activity-name (concat
+;;                            ceamx-activity-name-prefix
+;;                            ceamx-news-activity-name))
+;;           (new-activity-p (not (member activity-name (activities-names)))))
+;;     (when new-activity-p
+;;       (make-activities-activity :name activity-name))
+;;     (let ((activity (activities-named activity-name)))
+;;       (activities-switch activity)
+;;       (activities-set activity :state nil)
+;;       (unless (memq (buffer-local-value 'major-mode
+;;                       (window-buffer (selected-window)))
+;;                 '( elfeed-show-mode
+;;                    elfeed-search-mode))
+;;         (elfeed))
+;;       (activities-save activity :defaultp new-activity-p))))
+
+(provide 'ceamx-news)
+;;; ceamx-news.el ends here
