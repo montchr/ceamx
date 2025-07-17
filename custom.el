0f4dc82fba430e7df2238faa5a8c912f82218cce
Chris Montgomery <chmont@protonmail.com>
2025-07-17 17:41:40 -0400

    fix(org:agenda): remove `org-agenda-files` from `custom.el`

--- custom.el
+++ custom.el
@@ -3,11 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files
-    '("/home/cdom/Documents/notes/g2d/meta.org"
-       "/home/cdom/Documents/notes/g2d/people.org"
-       "/home/cdom/Documents/notes/g2d/todo.org"
-       "/home/cdom/Documents/notes/g2d/work.org"))
  '(safe-local-variable-values
     '((apheleia-formatter . alejandra)
        (org-refile-targets (nil :maxlevel . 6))
