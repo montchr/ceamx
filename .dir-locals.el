b7206116b043dc24ec694328ca268aaa55601cf5
Chris Montgomery <chmont@protonmail.com>
2025-07-25 18:13:12 -0400

    chore(meta): commit local dictionary changes

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,7 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((magit-status-mode . ((magit-todos-exclude-globs . (".git/" "lisp/*.el"))))
+((nil . ((jinx-dir-local-words . "configurated")))
+ (magit-status-mode . ((magit-todos-exclude-globs . (".git/" "lisp/*.el"))))
  (org-mode . ((org-archive-location . "graveyard/%s.archive::datetree/")
               (tab-width . 8))))
