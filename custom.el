d2ee132f0efe5e6350241e23e95f9e73a99a378e
Chris Montgomery <chmont@protonmail.com>
2025-07-25 17:14:13 -0400

    feat(vcs): add jujutsu integration

--- custom.el
+++ custom.el
@@ -4,12 +4,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values
-    '((apheleia-formatter . alejandra)
-       (org-refile-targets (nil :maxlevel . 6))
-       (apheleia-formatter . nixfmt)
+    '((org-refile-targets (nil :maxlevel . 6))
+       (lentic-init . lentic-orgel-org-init)
        (eval load-file "./ceamx-dev-loader.el")
-       (eval add-hook 'after-save-hook #'org-gfm-export-to-markdown t
-         t))))
+       (eval add-hook 'after-save-hook #'org-gfm-export-to-markdown t t))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
