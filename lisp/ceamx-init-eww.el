e237b8245c08764ce83253e5608f15db90d82300
Chris Montgomery <chmont@protonmail.com>
2025-07-27 12:15:11 -0400

    fix(eww): prevent long startup delay

--- lisp/ceamx-init-eww.el
+++ lisp/ceamx-init-eww.el
@@ -2,33 +2,42 @@
 
 (require 'ceamx-lib)
 
-(setopt browse-url-browser-function 'eww-browse-url)
+(setup browse-url
+  (:when-loaded
+    (:with-feature eww
+      (setopt browse-url-browser-function #'eww-browse-url))))
 
-(setopt shr-use-colors t)
-(setopt shr-folding-mode t)
-(setopt shr-bullet "• ")
+(setup shr
+  (:when-loaded
+    (setopt shr-use-colors t)
+    (setopt shr-folding-mode t)
+    (setopt shr-bullet "• ")))
 
-(setopt eww-search-prefix "https://duckduckgo.com/html?q=")
+(setup eww
+  (:also-load ceamx-eww)
+  (:bind "," '("scroll down" . scroll-up-command)
+         "." '("scroll up" . scroll-down-command)
+         "o" '("open link" . link-hint-open-link))
+  (:with-map ceamx-launch-prefix
+    (:bind "b" #'eww))
+  (:with-feature ceamx-eww
+    (:with-map ceamx-launch-prefix
+      (:bind "W" #'ceamx/eww-wiki)))
+  ;; Avoid loading this feature until needed.
+  (:when-loaded
+    (setopt eww-search-prefix "https://duckduckgo.com/html?q=")))
 
-;; HTTP headers may contain user information, which we can limit as needed.
-;; When providing a list of symbols, the symbols indicate what NOT to send.
-;; TODO: Move this elsewhere, as it probably affects other HTTP requests.
-(setopt url-privacy-level '(email lastloc))
+(setup url
+  ;; HTTP headers may contain user information, which we can limit as needed.
+  ;; When providing a list of symbols, the symbols indicate what NOT to send.
+  ;; TODO: Move this elsewhere, as it probably affects other HTTP requests.
+  (setopt url-privacy-level '(email lastloc)))
 
-(keymap-set ceamx-launch-prefix "b" #'eww)
-(keymap-set ceamx-launch-prefix "W" #'ceamx/eww-wiki)
-
-(after! eww
-  (define-keymap :keymap eww-mode-map
-    "," '("scroll down" . scroll-up-command)
-    "." '("scroll up" . scroll-down-command)
-    "o" '("open link" . link-hint-open-link)))
-
-(use-feature! ceamx-eww
-  :after eww
-  :functions (ceamx-eww-rerender)
-  :init
-  (add-hook 'eww-after-render-hook #'ceamx-eww-rerender))
+(setup eww
+  (:when-loaded
+    (:with-feature ceamx-eww
+      (:with-function #'ceamx-eww-rerender
+        (:hook-into eww-after-render-hook)))))
 
 (provide 'ceamx-init-eww)
 ;;; ceamx-init-eww.el ends here
